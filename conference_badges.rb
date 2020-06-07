63a66df02603886b9a48f398f06a82cce567ddf0
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-07 17:17:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,9 @@
 def badge_maker (name)
-  return "Hello, my name is #{name}"
+  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator (names)
   names.each do |name|
-    puts "Hello, my name is #{name}"
+    puts "Hello, my name is #{name}."
   end
 end
