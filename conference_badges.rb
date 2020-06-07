f3b2f24c6c86ac1b3e6b32132827f4c0849fb6fe
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-07 17:23:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,7 @@ end
 def assign_rooms (names)
   assignment_array = []
   names.each_with_index do |name, index|
+    index += 1
     assignment_array << "Hello, #{name}! You'll be assigned to room #{index}"
   end
   assignment_array  
